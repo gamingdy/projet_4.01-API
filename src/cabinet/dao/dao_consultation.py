abca0429238ef332c18f76236c568579a13d435d
Antoine Delagnes <antoine.delagnes@etu.iut-tlse3.fr>
2024-03-13 15:31:17 +0100

    feat: dao consult et medecin

--- src/cabinet/dao/dao_consultation.py
+++ src/cabinet/dao/dao_consultation.py
@@ -1,1 +1,50 @@
-class DaoConsultation: ...
+from .connection import Connection
+class DaoConsultation: 
+    def __init__(self):
+        self.db = Connection().get_connection()
+    
+    def get_consultation(self,id_consultation):
+        cursor = self.db.cursor()
+        cursor.execute(
+            "SELECT * FROM consultation WHERE id=%s",
+            (id_consultation,),
+        )
+        usager=cursor.fetchtone()
+        cursor.close()
+        return(usager)
+
+    def get_consultations(self):
+        cursor = self.db.cursor()
+        cursor.execute(
+            "SELECT * FROM consultation",
+        )
+        usagers=cursor.fetchall()
+        cursor.close()
+        return(usagers)
+
+    def add_consultation(self,date,heure,duree,medecin,usager):
+        cursor = self.db.cursor()
+        cursor.execute(
+            "INSERT INTO consultation (date_consult, heure_consult, duree_consult, id_medecin, id_usager) VALUES (%s,%s,%s,%s,%s)",
+            (date,heure,duree,medecin,usager,),
+        )
+        self.db.commit()
+        cursor.close()
+    
+    def update_consultation(self,id,date,heure,duree,medecin,usager):
+        cursor = self.db.cursor()
+        cursor.execute(
+            "UPDATE consultation SET date_consult=%s, heure_consult=%s, duree_consult=%s, id_medecin=%s, id_usager=%s WHERE id=%s",
+            (date,heure,duree,medecin,usager,id,),
+        )
+        self.db.commit()
+        cursor.close()
+       
+    def delete_consultation(self,id):
+        cursor = self.db.cursor()
+        cursor.execute(
+            "DELETE FROM consultation WHERE id=%s",
+            (id,),
+        )
+        self.db.commit()
+        cursor.close()
--- src/cabinet/dao/dao_medecin.py
+++ src/cabinet/dao/dao_medecin.py
@@ -1,1 +1,51 @@
-class DaoMedecin: ...
+from .connection import Connection
+class DaoMedecin: 
+    def __init__(self):
+        self.db = Connection().get_connection()
+    
+    def get_medecin(self,id_medecin):
+        cursor = self.db.cursor()
+        cursor.execute(
+            "SELECT * FROM medecin WHERE id=%s",
+            (id_medecin,),
+        )
+        usager=cursor.fetchtone()
+        cursor.close()
+        return(usager)
+
+    def get_medecins(self):
+        cursor = self.db.cursor()
+        cursor.execute(
+            "SELECT * FROM medecin",
+        )
+        usagers=cursor.fetchall()
+        cursor.close()
+        return(usagers)
+
+    def add_medecin(self,civilite,nom,prenom):
+        cursor = self.db.cursor()
+        cursor.execute(
+            "INSERT INTO medecin (civilite,nom,prenom) VALUES (%s,%s,%s)",
+            (civilite,nom,prenom,),
+        )
+        self.db.commit()
+        cursor.close()
+    
+    def update_medecin(self,id,civilite,nom,prenom):
+        cursor = self.db.cursor()
+        cursor.execute(
+            "UPDATE medecin SET civilite=%s, nom=%s, prenom=%s WHERE id=%s",
+            (civilite,nom,prenom,id,),
+        )
+        self.db.commit()
+        cursor.close()
+       
+    def delete_medecin(self,id):
+        cursor = self.db.cursor()
+        cursor.execute(
+            "DELETE FROM medecin WHERE id=%s",
+            (id,),
+        )
+        self.db.commit()
+        cursor.close()
+
