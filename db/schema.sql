2d12976f1df7719fc2bf9b360d84f009412a4411
Gamingdy <christgik12@gmail.com>
2024-03-13 14:05:22 +0100

    feat: création de la configuration docker

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE usager(
-   id_usager INT AUTO_INCREMENT,
+   id INT AUTO_INCREMENT,
    civilite VARCHAR(50) NOT NULL,
    nom VARCHAR(50) NOT NULL,
    prenom VARCHAR(50) NOT NULL,
@@ -10,28 +10,28 @@ CREATE TABLE usager(
    date_nais DATE NOT NULL,
    lieu_nais VARCHAR(50) NOT NULL,
    num_secu CHAR(15) NOT NULL,
-   CONSTRAINT PK_usager PRIMARY KEY(id_usager),
+   CONSTRAINT PK_usager PRIMARY KEY(id),
    CONSTRAINT AK_usager UNIQUE(num_secu)
 );
 
 CREATE TABLE medecin(
-   id_medecin INT AUTO_INCREMENT,
+   id INT AUTO_INCREMENT,
    civilite VARCHAR(5) NOT NULL,
    nom VARCHAR(50) NOT NULL,
    prenom VARCHAR(50) NOT NULL,
-   CONSTRAINT PK_medecin PRIMARY KEY(id_medecin)
+   CONSTRAINT PK_medecin PRIMARY KEY(id)
 );
 
 CREATE TABLE consultation(
-   id_consult INT AUTO_INCREMENT,
+   id INT AUTO_INCREMENT,
    date_consult DATE NOT NULL,
    heure_consult TIME NOT NULL,
    duree_consult TINYINT NOT NULL,
    id_medecin INT NOT NULL,
    id_usager INT NOT NULL,
-   CONSTRAINT PK_consultation PRIMARY KEY(id_consult),
+   CONSTRAINT PK_consultation PRIMARY KEY(id),
    CONSTRAINT AK_consultation_idx2 UNIQUE(id_medecin, date_consult, heure_consult),
    CONSTRAINT AK_consultation_idx1 UNIQUE(id_usager, date_consult, heure_consult),
-   CONSTRAINT FK_consultation_medecin FOREIGN KEY(id_medecin) REFERENCES medecin(id_medecin),
-   CONSTRAINT FK_consultation_usager FOREIGN KEY(id_usager) REFERENCES usager(id_usager)
+   CONSTRAINT FK_consultation_medecin FOREIGN KEY(id_medecin) REFERENCES medecin(id),
+   CONSTRAINT FK_consultation_usager FOREIGN KEY(id_usager) REFERENCES usager(id)
 );
