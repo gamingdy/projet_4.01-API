93b00feb83a0192a964ee319354276013831a66c
Gamingdy <christgik12@gmail.com>
2024-03-20 14:08:13 +0100

    feat: ajout des bases du modele usager et medecin

--- src/cabinet/model/medecin.py
+++ src/cabinet/model/medecin.py
@@ -1,6 +1,15 @@
+from pydantic import BaseModel
+
+
 class Medecin:
     def __init__(self, id: int, civilite: str, nom: str, prenom: str):
         self.id = id
         self.civilite = civilite
         self.nom = nom
         self.prenom = prenom
+
+
+class MedecinBase(BaseModel):
+    civilite: str
+    nom: str
+    prenom: str
