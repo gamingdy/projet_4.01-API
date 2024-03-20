41c77064cae145d0ff6e484453b1e2847b44a39d
Gamingdy <christgik12@gmail.com>
2024-03-20 14:02:25 +0100

    feat: création de retour d'erreur custom

--- src/cabinet/main.py
+++ src/cabinet/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
 from src.cabinet.routers import (
     router_consultation,
@@ -8,6 +11,56 @@ from src.cabinet.routers import (
 )
 
 app = FastAPI()
+
+
+def cleared_loc(lst, value):
+    return [x for x in lst if x != value]
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(
+    request: Request, exc: RequestValidationError
+):
+    detail = exc.errors()
+    modified_detail = {"status": "error", "status_code": 500}
+    result = {}
+
+    for error in detail:
+        error_type = error["type"].capitalize()
+
+        if error_type in result:
+            result[error_type]["loc"].extend(error["loc"])
+        else:
+            result[error_type] = {
+                "loc": list(error["loc"]),
+                "msg": error["msg"],
+            }
+
+    for type_error in result:
+        loc = result[type_error]["loc"]
+        cleared = cleared_loc(loc, "body")
+        result[type_error]["loc"] = cleared
+
+    if "Missing" in result:
+        modified_detail["status_code"] = 400
+        modified_detail["status_message"] = (
+            f"Missing {','.join(result['Missing']['loc'])}. {result['Missing']['msg']}"
+        )
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content=jsonable_encoder(modified_detail),
+        )
+
+    if "String_type" in result:
+        modified_detail["status_code"] = 400
+        modified_detail["status_message"] = (
+            f"Invalid {','.join(result['String_type']['loc'])}. {result['String_type']['msg']}"
+        )
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content=jsonable_encoder(modified_detail),
+        )
+
 
 app.include_router(router_usager.router)
 app.include_router(router_consultation.router)
